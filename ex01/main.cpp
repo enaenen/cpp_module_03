279033842fcdb8f502c62d0a1a22073135bc436f
Woo Joo Chae <enaenen@naver.com>
2022-08-31 12:21:18 +0900

    main test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,11 +1,34 @@
 #include "ScavTrap.hpp"
 
 int main( void ) {
+	std::cout << "==================== Constructor START ===================="
+			  << std::endl;
+
 	ScavTrap sc1( "SCAV ONE" );
-	ScavTrap sc2( " BBBB " );
+	ScavTrap sc2( "BBBB" );
+
+	ScavTrap sc3( "C" );
+	ScavTrap sc4;
 	ClapTrap *ptr;
 
+	std::cout << "==================== Constructor END ===================="
+			  << std::endl
+			  << std::endl;
+	;
+	std::cout << "==================== CopyAssignment TEST ===================="
+			  << std::endl;
+	sc4 = sc3;
+	std::cout << std::endl;
+
 	ptr = &sc1;
-	sc1.attack( "XXX" );
-	ptr->attack( "XER" );
+	ptr->attack( "sc2" );
+
+	sc2.toString();
+	sc1.attack( "sc2" );
+	sc2.takeDamage( sc1.getAttackDamage() );
+	sc2.guardGate();
+
+	sc2.toString();
+	std::cout << "==================== Destructor Call ===================="
+			  << std::endl;
 }
